9f6f4aca5a4eadb54146579c797b15f8d9fade5d
weboracamp <weboracamp@gmail.com>
2026-09-13 23:24:09 +0300

    Delete src directory

--- src/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { supabase, isSupabaseConfigured } from './lib/supabase';
-export type { SupabaseClient } from '@supabase/supabase-js';
-
-export { supabase, isSupabaseConfigured };
-export default supabase;
-
